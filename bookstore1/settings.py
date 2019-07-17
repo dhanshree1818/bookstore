6414eb85cf16403d13d4f43f94c344ed98b96de2
dhanshree1818 <dhanshree1818@gmail.com>
2019-07-17 18:54:17 +0530

    Login Logout

--- bookstore1/settings.py
+++ bookstore1/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
    # 'social.apps.django_app.default',
-    'registration',
     'store',
+    'registration',
 
 ]
 
